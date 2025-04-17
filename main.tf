4cc892c47d635f3270a84b41acf800740b588259
Sourabh Chhabra <chhabra_sourabh@hotmail.com>
2025-04-17 19:31:25 +0530

    removed Provider files from Lab 20 Folders

--- main.tf
+++ main.tf
@@ -7,11 +7,10 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "7a0bf087-e2b9-4fb2-bb79-4ef863e0c025"
 }
 
 resource "azurerm_resource_group" "rg-01" {
-  name = "rg-ci-p-odoo-01"
+  name     = "rg-ci-p-odoo-01"
   location = "Central India"
 }
 
